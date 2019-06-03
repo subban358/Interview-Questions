3eafce58de1042c5a517e760275b80127861370b
Subham Banerjee <32572976+subban358@users.noreply.github.com>
2019-06-03 19:05:27 +0530

    Update BitWise.java

--- BitWise.java
+++ BitWise.java
@@ -1,3 +1,21 @@
+/*Evaluate a boolean expression represented as a string
+Given a string consisting of only 0, 1, A, B, C where
+A = AND
+B = OR
+C = XOR
+Calculate the value of the string assuming no order of precedence and evaluation is done from left to right.
+
+Constraints – The length of the string will be odd. It will always be a valid string.
+Example, 1AA0 will not be given as an input.
+
+Input : 1A0B1
+Output : 1
+1 AND 0 OR 1 = 1
+
+Input : 1C1B1B0A0
+Output : 0
+*/
+
 import java.util.Scanner;
 public class BitWise 
 {
