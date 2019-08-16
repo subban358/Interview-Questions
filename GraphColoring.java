a164cced21e0edf733ad4b53fe19820916a30b56
Subham Banerjee <32572976+subban358@users.noreply.github.com>
2019-08-16 18:52:18 +0530

    Update GraphColoring.java

--- GraphColoring.java
+++ GraphColoring.java
@@ -1,4 +1,4 @@
-public class GraphColoring
+public class Main
 {
     static boolean isSafe(int g[][],int k,int c,int x[],int n)
     {
@@ -12,33 +12,37 @@ public class GraphColoring
         return true;
         
     }
-    static void graphColor(int g[][],int k,int m,int x[],int n)
+    static boolean graphColor(int g[][],int k,int m,int x[],int n)
     {
+        if (k==n)
+        {
+            for(int h=0;h<n;h++)
+                System.out.printf("%d ", x[h]);
+            return true;
+        }
+            
+    
         for(int c=1;c<=m;c++)
         {
             if(isSafe(g,k,c,x,n))
             {
                 x[k]=c;
                 
-                if(k+1<n)
-                    graphColor(g,k+1,m,x,n);
-                else
-                {
-                    for(int h=0;h<n;h++)
-                        System.out.printf("%d ", x[h]);
-                    return;    
-                }    
-            }
+                if(graphColor(g,k+1,m,x,n))
+                    return true;
+            }        
         }
+        return false;
     }
+    
 	public static void main(String[] args)
 	{
 	    int m=3,n=4;
 		int g[][] = {
-		                {1,1,0,1},
 		                {1,1,1,1},
-		                {0,1,1,1},
-		                {1,1,1,1}
+		                {1,1,1,0},
+		                {1,1,1,1},
+		                {1,0,1,1}
 		            };
 	    int x[] = {0,0,0,0};	            
 	    graphColor(g,0,m,x,n);	            
